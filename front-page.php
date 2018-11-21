95ece547d49207593c662f86e9979b97099353c8
Tinkerererer <45240425+Tinkerererer@users.noreply.github.com>
2018-11-21 12:34:21 -0500

    First Upload HTML

---
+++ front-page.php
@@ -0,0 +1,82 @@
+<!-- Navbar -->
+<head>
+   <link rel="stylesheet" href="https://codepen.io/Tinkererer/pen/VVrovy.css">
+  <body>
+<!-- Navbar -->
+<div class="pagewrapper">
+<div class="topofpage FloatCenter">
+  <header class=" red NavbarDiv">
+   
+     <a href="http://172.16.2.250/staff-directory" class="Navbar hover-white">Staff Directory</a>
+    <a href="http://172.16.2.250/Staff" class="Navbar hover-white">Staff</a>
+    <a href="http://172.16.2.250/Students" class="Navbar hover-white">Students</a>
+    <a href="http://172.16.2.250/Parents" class="Navbar hover-white">Parents</a>
+    <a href="http://172.16.2.250/Schools" class="Navbar hover-white">Schools</a>
+    <a href="http://172.16.2.250/Announcements" class="Navbar hover-white">Announcements</a>    
+    <a href="http://172.16.2.250" class="Navbar white">Home </a>
+
+  </header>
+     </div>
+     <div class="slider-container pagewrapper-col">
+
+  <section class="slider-left">
+ <div id="icondiv"> </div>
+  <p class="font-slider-left-header">
+    ROSSVILLE CONSOLIDATED SCHOOLS
+
+</p>
+<p class="font-slider-left-text">
+1 Robert Egly Drive, Rossville, IN 46065
+  </p>
+    <p class="font-slider-left-text">
+Phone (765) 379-2990
+Fax (765) 379-3014
+  </p>
+
+  </section>
+   <section class="slider-right">
+   <div id="imgdiv"> </div>
+  
+  </section>
+       
+     </div>
+ <!-- Ending SLIDER -->
+ <!-- START QUICKLINKS -->
+<div class="quicklink-container">
+<div class="ql-sub-container">
+<div class="ql">
+
+
+<img src="http://p7cdn4static.sharpschool.com/UserFiles/Servers/Server_452651/Image/HomeScreen/QuickLinks/2019/ath2.png" class="img-base" alt="Athletics">  
+<img src="http://p7cdn4static.sharpschool.com/UserFiles/Servers/Server_452651/Image/HomeScreen/QuickLinks/2019/ath1.png" class="img-layer" alt="Athletics-After" title="Athletics">
+
+
+<img src="http://p7cdn4static.sharpschool.com/UserFiles/Servers/Server_452651/Image/HomeScreen/QuickLinks/2019/ath2.png" class="img-base" alt="Athletics">  
+<a href="https://rossvillehornets.weebly.com/" title="Athletics"><img src="http://p7cdn4static.sharpschool.com/UserFiles/Servers/Server_452651/Image/HomeScreen/QuickLinks/2019/ath1.png" class="img-layer" alt="Athletics-After" title="Athletics"></a>
+</div>
+  </div>
+</div>
+<!-- Ending QUICKLINKS -->
+<!-- START TWITTER -->
+<div class="columns">
+
+
+<div class="twitter-container">
+<a class="twitter-timeline" href="https://twitter.com/RossvilleSchool?ref_src=twsrc%5Etfw" data-height="500">Tweets by RossvilleSchool</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
+<!-- Ending TWITTER -->
+<!-- START SIDEPANEL -->
+<div class="sidepanel-container">
+<a class="twitter-timeline" href="https://twitter.com/RossvilleSchool?ref_src=twsrc%5Etfw" data-height="500">Tweets by RossvilleSchool</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
+<!-- END SIDEPANEL -->
+</div>
+<!-- NEWS -->
+<div class="news-container">
+<div class="news-content">
+qwdasd
+</div>
+</div>
+<!-- NEWS -->
+
+</div>
